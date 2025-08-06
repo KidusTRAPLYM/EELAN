e24a01787b9e0aca63396776ea6ed20d662f2a66
Your Name <you@example.com>
2025-08-05 21:04:19 -1200

    commited

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ async function connectDB() {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 3000000,
+      serverSelectionTimeoutMS: 30000,
     });
     console.log("MongoDB connected");
   } catch (err) {
